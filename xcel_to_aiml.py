2f1cb1a17b3e0682b7c2dc42dc6a40270dce4489
m.dinh10k <mdinh10k@gmail.com>
2018-11-30 19:05:35 +0700

    first

---
+++ xcel_to_aiml.py
@@ -0,0 +1,39 @@
+'''
+@date 29/11/2018
+@author Minh Dinh
+
+input: excel files
+output: aiml files
+
+'''
+
+'''
+    read excel files:
+        + input: excel
+        + ouptut: data as key:value
+'''
+
+#open excel file
+
+#read excel file
+
+
+'''
+    processing data as k:v
+        + input: data as k:v
+        + output: pattern : template
+'''
+# group questions that have the same answer 
+
+# analyze question >> remove stopwords, etc....
+
+
+
+
+'''
+    write pattern:template to aiml files
+'''
+
+# create aiml header n footer
+
+# write pattern : template
